1f52ecfe25326f5544126a3dceed27540a6d06e7
Matheus Santos <130078547+theuusc@users.noreply.github.com>
2024-02-06 19:11:04 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from '../src/components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
+import Footer from './components/Footer/Footer';
+import Main from './components/Main/Main';
+
+
+function App() {
+  return (
+    <div>
+      <Sidebar />
+      <Header />
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
